c0b431b802f289d4136438368a8ac16efa343a51
Farouk Belhocine <farouk.belhocine@itcomp-dz.com>
2024-12-18 09:55:50 +0100

    add: Tests project for testing our web app

---
+++ GestionConcoursApp/Controllers/CalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionConcoursApp.Controllers
+{
+    [ApiController]
+    [Route("api/calculator")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        [Route("add")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ GestionConcoursAppTests/Controllers/CalculatorControllerTests.cs
@@ -0,0 +1,22 @@
+﻿namespace GestionConcoursApp.Controllers.Tests
+{
+    [TestClass()]
+    public class CalculatorControllerTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            CalculatorController calculatorController = new CalculatorController();
+
+            int a = 10;
+            int b = 20;
+
+            int expected = 30;
+
+
+            var actual = calculatorController.Add(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
